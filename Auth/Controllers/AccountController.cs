eba78ae33935d9ef7badb39b005d8965ffdc1de0
КахаровЖ <@N215-2>
2020-02-12 21:05:33 +0600

    Add project files.

---
+++ Auth/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Auth.Interfaces;
+using Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auth.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthProvider AuthProvider;
+        public AccountController()
+        {
+            AuthProvider = new FormAuthProvider();
+        }
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (AuthProvider.Authenticate(user.Login, user.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "My"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect login or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Auth/Models/FormAuthProvider.cs
@@ -0,0 +1,23 @@
+﻿using Auth.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Auth.Models
+{
+    public class FormAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string userName, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(userName,password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+            }
+
+            return result;
+        }
+    }
+}
